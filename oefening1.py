8d6665d8de7354198c03a289ea7d3e359ba9dded
Jarne.Bourgois <Jarne.Bourgois@student.tsaam.be>
2024-10-04 11:50:04 +0200

    commit1

---
+++ oefening1.py
@@ -0,0 +1,9 @@
+#Maak een dictionary die de volgende informatie bevat over een persoon: naam, leeftijd, en woonplaats.
+# 1.	Voeg de informatie toe aan de dictionary.
+# 2.	Print de naam, leeftijd, en woonplaats afzonderlijk.
+
+persoon = {"naam":"jeff","leeftijd":24,"woonplaats":"Zerkehem"}
+
+print(persoon["naam"])
+print(persoon["leeftijd"])
+print(persoon["woonplaats"])
---
+++ oefening2.py
@@ -0,0 +1,7 @@
+# #Gebruik de dictionary van oefening 1.
+# 1.	Verander de leeftijd van de persoon.
+# 2.	Voeg een nieuw item toe voor het beroep.
+# 3.	Print de bijgewerkte dictionary.
+
+
+persoon = {"naam":"jeff","leeftijd":24,"woonplaats":"Zerkehem"}
